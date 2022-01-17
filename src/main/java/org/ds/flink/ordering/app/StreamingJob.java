0f1401affc6b17357e7ad1b7af48371fdb459f85
d-smith <doug.smith.mail@gmail.com>
2022-01-16 20:31:39 -0800

    Remove private statics, added calls to extracted

--- src/main/java/org/ds/flink/ordering/app/StreamingJob.java
+++ src/main/java/org/ds/flink/ordering/app/StreamingJob.java
@@ -2,18 +2,13 @@
 
 package org.ds.flink.ordering.app;
 
-import org.apache.flink.api.common.functions.AggregateFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
-import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
 import org.apache.flink.streaming.api.windowing.time.Time;
-import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.streaming.connectors.kinesis.FlinkKinesisConsumer;
 import org.apache.flink.streaming.connectors.kinesis.config.ConsumerConfigConstants;
-import org.apache.flink.util.Collector;
-import org.ds.flink.ordering.pojos.Mutation;
+import org.ds.flink.ordering.windows.AggregateSorter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -47,66 +42,10 @@ public class StreamingJob {
 				.flatMap(new MutationFlatmapper())
 				.keyBy(m -> m.id)
 				.timeWindow(Time.seconds(1))
-				//.aggregate(new MyAccumulator())
-				.process(new MyProcessWindowFunction())
+				.aggregate(new AggregateSorter())
 				.print();
 
 		// execute program
 		env.execute("Flink Streaming Java API Skeleton");
-	}
-
-	private static class MyProcessWindowFunction extends ProcessWindowFunction<Mutation, Mutation, String, TimeWindow> {
-		@Override
-		public void process(String key, Context context, Iterable<Mutation> iterable, Collector<Mutation> collector) throws Exception {
-			log.info("window process called for key {}", key);
-
-			List<Mutation> ml = new ArrayList<>();
-			iterable.forEach(m -> ml.add(m));
-
-			Collections.sort(ml, new Comparator<Mutation>() {
-				@Override
-				public int compare(Mutation o1, Mutation o2) {
-					return o1.timeStamp.compareTo(o2.timeStamp);
-				}
-			});
-
-			ml.forEach(m -> collector.collect(m));
-		}
-	}
-
-	private static class MyAccumulator implements AggregateFunction<Mutation, List<Mutation>, List<Mutation>> {
-
-		@Override
-		public List<Mutation> createAccumulator() {
-			log.info("create acc");
-			return new ArrayList<>();
-		}
-
-		@Override
-		public List<Mutation> add(Mutation mutation, List<Mutation> mutations) {
-			log.info("add");
-			mutations.add(mutation);
-			return mutations;
-		}
-
-		@Override
-		public List<Mutation> getResult(List<Mutation> mutations) {
-			log.info("get result");
-			Collections.sort(mutations, new Comparator<Mutation>() {
-				@Override
-				public int compare(Mutation o1, Mutation o2) {
-					return o1.timeStamp.compareTo(o2.timeStamp);
-				}
-			});
-
-			return mutations;
-		}
-
-		@Override
-		public List<Mutation> merge(List<Mutation> mutations, List<Mutation> acc1) {
-			log.info("merge");
-			mutations.addAll(acc1);
-			return mutations;
-		}
 	}
 }
--- src/main/java/org/ds/flink/ordering/app/StreamingJob2.java
+++ src/main/java/org/ds/flink/ordering/app/StreamingJob2.java
@@ -7,15 +7,12 @@ import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
-import org.apache.flink.streaming.api.windowing.assigners.SlidingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
-import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.streaming.connectors.kinesis.FlinkKinesisConsumer;
 import org.apache.flink.streaming.connectors.kinesis.config.ConsumerConfigConstants;
-import org.apache.flink.util.Collector;
 import org.ds.flink.ordering.pojos.Mutation;
+import org.ds.flink.ordering.windows.WindowSorter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -63,20 +60,10 @@ public class StreamingJob2 {
 				.keyBy(mutation -> mutation.id)
 				//.timeWindow(Time.seconds(5))
 				.window(TumblingEventTimeWindows.of(Time.seconds(5)))
-				.process(new MyProcessWindowFunction())
+				.process(new WindowSorter())
 				.print();
 
 		// execute program
 		env.execute("Flink Streaming Java API Skeleton");
-	}
-
-	private static class MyProcessWindowFunction extends ProcessWindowFunction<Mutation, Mutation, String, TimeWindow> {
-		@Override
-		public void process(String key, Context context, Iterable<Mutation> iterable, Collector<Mutation> collector) throws Exception {
-			log.info("window process called for key {}", key);
-			for(Mutation m: iterable) {
-				collector.collect(m);
-			}
-		}
 	}
 }
